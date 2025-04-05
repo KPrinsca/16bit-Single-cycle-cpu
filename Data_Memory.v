12ec3c3e74261b4cc732a1f5a33fbe6eb9bebdb4
Prinsca Nayram <88563576+KPrinsca@users.noreply.github.com>
2025-04-05 04:19:00 -0400

    Create Data_Memory.v

---
+++ Data_Memory.v
@@ -0,0 +1,34 @@
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Data_Memory(
+    input [15:0] DMem_In, // ALU Output
+    input [15:0] Data_Write, //From Register file
+    input Mem_Write,
+    input Mem_Read,
+    input clk,
+    output [15:0] DataM_out
+    );
+    
+
+ reg 	[7:0] 	Memory[0:255];
+ 
+   // Memory read operation (handling byte addressing)
+    // Memory read operation (handling byte addressing)
+    	    assign DataM_out = (Mem_Read) ? {Memory[DMem_In],Memory[DMem_In+1]} : 16'b0;
+    always 	@(negedge clk)		   //Memory write
+   		begin
+   		 if (Mem_Write) begin
+    Memory[DMem_In] <= Data_Write[15:8];
+      Memory[DMem_In+1] <= Data_Write[7:0];
+    end
+  end
+  
+ 
+
+  
+
+   		
+    
+endmodule
