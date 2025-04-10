2512e4ea2c82c0b5fb9ea1c2512b47ed68ee113d
Prinsca Nayram <88563576+KPrinsca@users.noreply.github.com>
2025-04-10 14:00:33 -0400

    Update CPU_TOP_test.v

--- CPU_TOP_test.v
+++ CPU_TOP_test.v
@@ -3,29 +3,33 @@ module CPU_16_test();
 
    reg clk;
    reg reset;
-   wire [15:0]ALU_out, pc_out,DATA_R1,DATA_R2, IM_output;
-  wire [3:0] ALUop;
+   wire [15:0]ALU_out, Program_cout,DATA_R1,DATA_R2, IM_output;
+  wire [3:0] ALUop,Rd;
+  wire ALU_z,Read_Memory,B_int;
    
  
-CPU_16_TOP uut(clk,reset,ALU_out,pc_out,DATA_R1,DATA_R2,IM_output,ALUop);
+CPU_16_TOP uut(clk,reset,ALU_out,Program_cout,DATA_R1,DATA_R2,IM_output,ALUop,Rd,ALU_z,Read_Memory,B_int);
 
 initial begin
-     
+        // Initialize Signals
         clk = 1;
         reset = 1;
         #5
         reset =0;
-        #100;   
+        #500;  
     end
 
-    always begin
-    #5 clk = ~clk; 
+ // Generate Clock Signal (50% Duty Cycle)
+ always begin
+    #5 clk = ~clk; // 10ns period (100MHz CPU)
     end
 
 
-always@(posedge clk) begin
-       $display("Time=%0t ALU_Out=%b | DATA_R2=%b | IM_output=%b | ALUop =%b", 
-                  $time, ALU_out, DATA_R2,IM_output,ALUop);
+
+
+   always@(negedge clk) begin
+       $display("Time=%0t ALU_Out=%b | DATA_R2=%b | IM_output=%b | ALUop =%b|ALU_zero=%b", 
+                  $time, ALU_out, DATA_R2,IM_output,ALUop,ALU_z);
              end     
 
 
