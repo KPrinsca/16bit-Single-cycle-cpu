cce57a19c9f24683bbd34e5fbf15c70f39c9ad3e
Prinsca Nayram <88563576+KPrinsca@users.noreply.github.com>
2025-04-10 17:19:35 -0400

    Update Data_Memory.v

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,42 +1,48 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module Data_Memory(
     input [15:0] DMem_In, // ALU Output
     input [15:0] Data_Write, //From Register file
     input Mem_Write,
     input Mem_Read,
     input clk,
-    output [15:0] DataM_out
+    output reg[15:0] DataM_out
     );
     
+//    reg 	[15:0] 	Memory[63:0];
+ reg 	[7:0] 	Memory[255:0];
+ 
+    integer i;
 
-    reg 	[7:0] 	Memory[255:0];
+wire [7 : 0] Mem_addr = DMem_In[7:0]; 
 
-integer i;
 initial begin
+  
   for (i = 0; i < 256; i = i + 1) begin
     Memory[i] = 8'b0;
   end
 end
 
-    
-   // Memory read operation (handling byte addressing)
-    // Memory read operation (handling byte addressing)
-    	    assign DataM_out = (Mem_Read) ? {Memory[DMem_In],Memory[DMem_In+1]} : 16'b0;
-    always 	@(negedge clk)		   //Memory write
+always@(*) begin
+      if(Mem_Read) begin
+ DataM_out = {Memory[Mem_addr],Memory[Mem_addr+1]};
+      end 
+      else begin
+            DataM_out = 16'b0; 
+        end
+
+end
+    always 	@(negedge  clk)		   //Memory write
    		begin
    		 if (Mem_Write) begin
-    Memory[DMem_In] <= Data_Write[15:8];
-      Memory[DMem_In+1] <= Data_Write[7:0];
+    Memory[Mem_addr] <= Data_Write[15:8];
+      Memory[Mem_addr+1] <= Data_Write[7:0];
     end
   end
   
- 
+  // Memory read operation (handling byte addressing)
+    // Memory read operation (handling byte addressing)
 
-  
-
-   		
     
 endmodule
