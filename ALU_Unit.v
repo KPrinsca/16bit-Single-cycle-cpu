5ecb294ecd6476ae21500fcfcf02781220c6290d
Prinsca Nayram <88563576+KPrinsca@users.noreply.github.com>
2025-04-13 16:02:14 -0400

    Update ALU_Unit.v

--- ALU_Unit.v
+++ ALU_Unit.v
@@ -11,7 +11,7 @@ module ALU_Unit(
          case(ALU_cont)
          4'b0000: ALU_output <= ALU_InA + ALU_InB;  
          4'b0001:  ALU_output <= ALU_InA - ALU_InB; 
-         4'b0010:  ALU_output <= ALU_InB << ALU_InA[3:0];  
+         4'b0010:  ALU_output <= ALU_InB << ALU_InA;  
          4'b0011:  ALU_output <= ALU_InA & ALU_InB;   
          default:  ALU_output <= 16'b0000000000000000;              
          endcase
